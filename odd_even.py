af325c746945427e25080f0d649d46e55d88d3af
MattRahlfs <43938137+MattRahlfs@users.noreply.github.com>
2023-03-11 19:38:11 -0700

    Add files via upload

--- odd_even.py
+++ odd_even.py
@@ -1,20 +1,38 @@
 #detect if a user generated value is odd or even
 
-
-
+# create a loop to ask for input
 
 while True:
-    input_value=float(0)
+# create a function to detect if the passsed variable is odd or even and output the answer
+    def is_even(input_value):
+        if input_value % 2 == 0:
+            print ("The input is even.")
+            return True
+        else:
+            print ("The input is odd.")
+            return False
     
-    if input_value == 0:
-        input_value= float(input("Enter a number\n"))
-    
-    if input_value % 2 == 0:
-        print("this number is even")
+#get input from user
+    def get_input():
         
-        break
+        input_value = 0
+        try:
+            input_value = int(input("Enter a real number to detect if it is even or odd.\n"))
+            
+        except ValueError:
+            get_input()
+        
+        return input_value
+            
+        
+        
+    is_even(get_input())
+    
+#do you want to go again? break the loop
+    end_game = input(str("Do you want to play again y/n."))
+    if end_game == "y":
+        continue
     else:
-        print ("this number is odd")
         break
     
-    
+print("Thanks for playing")
