a23abb43e93ea136232ceebbf2065e4dddcf6e73
MattRahlfs <43938137+MattRahlfs@users.noreply.github.com>
2023-03-12 23:18:46 -0600

    Add files via upload

--- rogue_like.py
+++ rogue_like.py
@@ -1,16 +1,10 @@
 import time
 
-
-class adventurer():
-    def __init__(self, name, weight, height, race, typeofclass):
-        self.name = name
-        self.weight = weight
-        self.height = height
-        self.race = race
-        self.typeofclass = typeofclass
-    
-    
-
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+ 
 
 def slow_print(sleep_time, string_to_print):
     time.sleep(sleep_time)
@@ -28,13 +22,71 @@ def typingInput(text):
     time.sleep(0.05)
   value = input()  
   return value  
+
+
+    
+
+
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
   
+  
+
+class adventurer():
+    def __init__(self, name, weight, height, race, typeofclass):
+        self.name = name
+        self.weight = weight
+        self.height = height
+        self.race = race
+        self.typeofclass = typeofclass
+        self.inventory = []
+        self.stats = {
+            'Health': 0,
+            'Strength': 0,
+            'Wisdom': 0
+                      }
+    
+    def get_inventory(self):
+        return self.inventory
+    
+    def add_to_inventory(self, item):
+        self.inventory.extend(item)
+        
+    def remove_from_inventory(self, item):
+        self.inventory.remove(item)
+        
+    def add_stat(self, stat, value):
+        self.stats[stat] += value
+        
+    def remove_stat(self, stat, value):
+        self.stats[stat] -= value
+    
+    def get_stats(self):
+        return self.stats
+    
+    
+class enemy():
+    def __init__(self, name, attacktype, health):
+        self.name = name
+        self.attacktype = attacktype
+        self.health = health
+        
+    
+
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+####################################################################################################################################
+ 
 
     
     
     
 slow_print(0,["Welcome to  --- GAME ---."])    
 slow_print(.8,["Lets create your character... "])
+
 
 name = str(typingInput('What do you want to be named?\n'))
 weight = float(typingInput('What is your weight in pounds?\n'))
@@ -44,4 +96,14 @@ typeofclass = str(typingInput('What class do you want to be? (Warrior, Priest, R
 
 character = adventurer(name, weight, height, race, typeofclass)
 
-print(character.name, character.race, character.typeofclass)
+
+
+if character.typeofclass.lower() == 'priest':
+    character.add_stat('Health', 10)
+    character.add_stat('Strength', 3)
+    character.add_stat('Wisdom', 15)
+    
+
+print(character.get_stats())
+
+
