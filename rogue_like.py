93e6b94a5fea5b4f1bae1b208d76fe82293fe5d6
MattRahlfs <43938137+MattRahlfs@users.noreply.github.com>
2023-03-13 19:36:28 -0600

    Add files via upload

--- rogue_like.py
+++ rogue_like.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 ####################################################################################################################################
 ####################################################################################################################################
@@ -45,7 +46,8 @@ class adventurer():
         self.stats = {
             'Health': 0,
             'Strength': 0,
-            'Wisdom': 0
+            'Wisdom': 0,
+            'Defence': 0
                       }
     
     def get_inventory(self):
@@ -66,14 +68,39 @@ class adventurer():
     def get_stats(self):
         return self.stats
     
+    def attack(self, enemy):
+        hit = self.stats['Wisdom'] - enemy.stats['Defence']
+        hit = random.randrange(1,hit+1)
+        if hit < 1:
+            hit = 1
+        return hit
+        
+
+    
     
 class enemy():
     def __init__(self, name, attacktype, health):
         self.name = name
         self.attacktype = attacktype
         self.health = health
+        self.stats = {
+            'Health': 0,
+            'Strength': 0,
+            'Wisdom': 0,
+            'Defence': 0
+                      }
         
+    def add_stat(self, stat, value):
+        self.stats[stat] += value
+        
+    def remove_stat(self, stat, value):
+        self.stats[stat] -= value
     
+    def get_stats(self):
+        return self.stats
+    
+        
+        
 
 ####################################################################################################################################
 ####################################################################################################################################
@@ -95,15 +122,32 @@ race = str(typingInput('What is your race? (Human, Elf, Dwarf, Orc)\n'))
 typeofclass = str(typingInput('What class do you want to be? (Warrior, Priest, Rogue)\n'))
 
 character = adventurer(name, weight, height, race, typeofclass)
-
+orc = enemy('orc1', 'melee', 10)
 
 
 if character.typeofclass.lower() == 'priest':
     character.add_stat('Health', 10)
     character.add_stat('Strength', 3)
     character.add_stat('Wisdom', 15)
+
+
+orc.add_stat('Health', 10)
+orc.add_stat('Strength', 10)
+orc.add_stat('Defence', 10)
+
+
+while True:
+    
+    choice = int(input('what do you want to do? 1 2 3\n'))
+    
+    if choice == 1:
+        damage = character.attack(orc)
+        orc.remove_stat('Health', damage)
+        print(orc.stats)
+    
+ 
+ 
     
 
-print(character.get_stats())
 
 
