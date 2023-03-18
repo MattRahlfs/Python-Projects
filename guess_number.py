954b05a7030778a489007c46ca902d5ebf4d28db
MattRahlfs <43938137+MattRahlfs@users.noreply.github.com>
2023-03-17 19:51:15 -0600

    Add files via upload

--- guess_number.py
+++ guess_number.py
@@ -3,7 +3,7 @@ import random
 
 
 Guess_number = random.randint(0, 10)
-print (Guess_number)
+#print (Guess_number)
 
 def get_input():
     input_value = 0
@@ -17,24 +17,39 @@ def get_input():
             continue
         
         
-def start():
+def start(userorcomputer):
     
     attempt_counter = 1
-
-    while True:
-        input_value = get_input()
-        
-        if input_value == Guess_number:
-            print ("You win and it only took: " + str(attempt_counter) + " attempts to get it right!")
-            break
-        elif input_value != Guess_number:
-            attempt_counter +=1
+    if userorcomputer == 0:
+        while True:
+            input_value = get_input()
             
-            if str(input("That is not correct, do you want to keep trying? y/n \n")) == 'y':
-                continue
-            else:
+            if input_value == Guess_number:
+                print ("You win and it only took: " + str(attempt_counter) + " attempts to get it right!")
                 break
+            elif input_value != Guess_number:
+                attempt_counter +=1
+                
+                if str(input("That is not correct, do you want to keep trying? y/n \n")) == 'y':
+                    continue
+                else:
+                    break
+    elif userorcomputer == 1:
+        valtoguess =  int(input('Enter the number you want to computer to guess (it must be between 0 an 10) '))
+        computerguess = 0
+        while computerguess != valtoguess:
             
-        
-start()
-    
+            if computerguess != valtoguess:
+                computerguess = random.randint(0,10)
+                attempt_counter +=1
+                print('The computer guess is ' + str(computerguess))
+            
+        print('It took the Computer ' + str(attempt_counter) + ' tries to get the correct number')
+    else:
+        return None
+            
+userorcomputer = int(input("Press 0 if you want to guess the number, or press 1 to have the computers guess your number "))     
+       
+
+start(userorcomputer)
+
