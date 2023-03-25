7da8fd7aa87402cc503763e945bcbd16e647d97c
MattRahlfs <43938137+MattRahlfs@users.noreply.github.com>
2023-03-25 15:30:00 -0600

    Add files via upload
    added an interface with tkinter
    the user can now interact with the list with a gui, there is a list a user can select from and buttons to modify the list

--- todo.py
+++ todo.py
@@ -1,21 +1,97 @@
 import os
 import json
 import time
+import tkinter as tk
 
 class task():
     def __init__ (self, description):
         self.description = description
         self.time = str(time.strftime("%H:%M:%S"))
-        
-    def convertToJSON():
-        
-        pass
-            
-def mainMenu():
     
-    while True:
-        userInput = input("Press 1 to list the tasks\nPress 2 to create a a new task\nPress 3 to update a task\nPress 4 to delete a task\nPress q to quit\nEnter an option: ")
+class todo_gui():
+    def __init__ (self):
+
+        def updateListBox():
+            tasksListBox.delete(0,tk.END)
+            todoList = jsonFileIO('', 'r', 'todo.json')
+            for eachTask in todoList['Tasks']:
+                tasksListBox.insert(tk.END, eachTask['Description'])   
+            tasksListBox.pack(pady=10)
+            
+        def createNewTask():
+            if newTaskDescriptionText.get() == '':
+                pass
+            else:
+                taskDetail = newTaskDescriptionText.get()
+                newTask = task(taskDetail)
+                todoList = jsonFileIO('', 'r', 'todo.json')
+                todoList['Tasks'].append({'Description': newTask.description, 'Time': newTask.time})
+                jsonFileIO(todoList, 'w', 'todo.json')
+                newTaskDescriptionText.delete(0, tk.END)
+                updateListBox()
+            
+        def updateExistingTask():
+            if updateTaskDescriptionText.get() == '':
+                pass
+            else:
+                todoList = jsonFileIO('', 'r', 'todo.json')
+                todoList['Tasks'][tasksListBox.curselection()[0]]['Description'] = updateTaskDescriptionText.get()
+                jsonFileIO(todoList, 'w', 'todo.json')
+                updateTaskDescriptionText.delete(0, tk.END)
+                updateListBox()
+            
+            
+        def deleteExistingTask():
+            try:
+                todoList = jsonFileIO('', 'r', 'todo.json')
+                todoList['Tasks'].pop(tasksListBox.curselection()[0])
+                jsonFileIO(todoList, 'w', 'todo.json')
+                updateListBox()
+            except IndexError:
+                pass
+            
+            
+        def closeTodoList():
+            pass
+        root = tk.Tk()
+        root.title('Todo List')
+        root.geometry('400x400')
+        root.resizable(False,False)
+        root.eval('tk::PlaceWindow . center')
         
+        todoList = jsonFileIO('', 'r', 'todo.json')
+        listBoxFrame = tk.Frame(root)
+        verticalScrollBar = tk.Scrollbar(listBoxFrame, orient=tk.VERTICAL)
+        tasksListBox = tk.Listbox(listBoxFrame, width=50, height=10,selectmode=tk.SINGLE, yscrollcommand=verticalScrollBar.set)
+        verticalScrollBar.config(command=tasksListBox.yview)
+        verticalScrollBar.pack(side=tk.RIGHT, fill=tk.Y)
+        listBoxFrame.pack()
+        updateListBox()
+        
+        newTaskLabel = tk.Label(root, text='Enter the description of a new task below')
+        newTaskLabel.pack(pady=5)
+        newTaskDescriptionText = tk.Entry(root)
+        newTaskDescriptionText.pack()
+        
+        
+        createTaskButton = tk.Button(root, text="ADD", command=createNewTask)
+        createTaskButton.pack()
+        
+        deleteTaskButton = tk.Button(root, text="DELETE", command=deleteExistingTask)
+        deleteTaskButton.pack(pady=5)
+        
+        updateTaskLabel = tk.Label(root, text='Enter the new description below')
+        updateTaskLabel.pack(pady=5)
+        updateTaskDescriptionText = tk.Entry(root)
+        updateTaskDescriptionText.pack()
+        updateTaskButton = tk.Button(root, text="Update", command=updateExistingTask)
+        updateTaskButton.pack(pady=5)
+        
+        root.mainloop()
+                        
+def mainMenu():
+  while True:
+        userInput = input("Press 1 to list the tasks\nPress 2 to create a a new task\nPress 3 to update a task\nPress 4 to delete a task\nPress q to quit\nEnter an option: ")
         if userInput == '1':
             displayTasks()
         elif userInput == '2':
@@ -32,11 +108,9 @@ def mainMenu():
             print("\nEnter a correct value\n")
             mainMenu()
         else:
-            break
+            break      
         
-        
-def jsonFileIO(data, readOrWrite, fileName):
-        
+def jsonFileIO(data, readOrWrite, fileName):     
     if readOrWrite == 'r':
         with open (str(fileName), str(readOrWrite)) as fileIO:
             readFile = json.load(fileIO)    
@@ -44,14 +118,10 @@ def jsonFileIO(data, readOrWrite, fileName):
     elif readOrWrite == 'w':
         with open (str(fileName), str(readOrWrite)) as fileIO:
             json.dump(data, fileIO)
-
-        
-
+    
 def createFileStructure():
     currentWorkingDirectory = os.getcwd()
-    todoDirectory =  currentWorkingDirectory + '\\todo\\'
-    
-    
+    todoDirectory =  currentWorkingDirectory + '\\todo\\'   
     if not os.path.exists(todoDirectory + 'todo.json'):
         if not os.path.exists(todoDirectory):
             os.mkdir('todo')
@@ -64,70 +134,35 @@ def createFileStructure():
             jsonFileIO(data, 'w', 'todo.json') 
     os.chdir(todoDirectory)
 
-
-
 def displayTasks():
-    
     todoList = jsonFileIO('', 'r', 'todo.json')
     print ('\n','Task:', '               ', 'Time created:', '\n')
     for eachTask in todoList['Tasks']:
-        
         print (eachTask['Description'], '               ',eachTask['Time'])
-    
     print('\n')
-    
-    
-    
+     
 def createTasks():
     taskDetail = input("Enter the task you want to add to the list  ")
     newTask = task(taskDetail)
-    
     todoList = jsonFileIO('', 'r', 'todo.json')
-    
     todoList['Tasks'].append({'Description': newTask.description, 'Time': newTask.time})
-    
     jsonFileIO(todoList, 'w', 'todo.json')
-    
     displayTasks()
         
-
-# allow user to update a task
-    #find the task based on name and update the description for that particular index  
 def updateTasks(taskIndexToUpdate):
-    
     todoList = jsonFileIO('', 'r', 'todo.json')
     taskDetail = input("Enter the task description you want to add to the list  ")
     newTask = task(taskDetail)
-     
     todoList['Tasks'][taskIndexToUpdate -1]['Description'] = newTask.description
-    
     jsonFileIO(todoList,'w','todo.json')
-    
     displayTasks()
-     
-    pass
-
     
 def deleteTask(taskIndexToDelete):
-    
     todoList = jsonFileIO('', 'r', 'todo.json')
-    
     todoList['Tasks'].pop(taskIndexToDelete -1)
-    
     jsonFileIO(todoList, 'w', 'todo.json')
-    
     displayTasks()
     
-    pass
-
-
-
+    
 createFileStructure()
-mainMenu()
-
-
-
-
-
-
-
+todo_gui()
